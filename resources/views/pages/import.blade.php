65bd216a902afd0545017cff95bfb16d15253cc0
Sam Pham <sam.h.pham@gmail.com>
2017-12-23 21:47:19 +0000

    Deployment test

--- resources/views/pages/import.blade.php
+++ resources/views/pages/import.blade.php
@@ -1,28 +1,21 @@
 @extends('layouts.app')
+@extends('layouts.navbar')
 
 @section('content')
-  <div class="row">
-    <div class="col-sm-3">
-      <div class="box box-primary">
-        <div class="box-header with-border">
-          <h3 class="box-title">Select a file to import</h3>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-          <form action="{{url('/csv_add')}}" method="post" enctype="multipart/form-data">
-            {{csrf_field()}}
-            <div class="form-group">
-              <label for="file">CSV file to upload</label>
-              <input type="file" name="file" id="file" class="form-control">
-            </div>
-
-            <div class="form-group">
-              <button class="btn btn-primary">
-                <i class="fa fa-upload"></i> Upload
-              </button>
-            </div>
+    <div class="row">
+        <div class="box box-primary">
+            <h3 class="box-title">Select a file to import</h3>
+            <form action="{{url('/csv_add')}}" method="post" enctype="multipart/form-data">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <label for="file">CSV file to upload</label>
+                    <input type="file" name="file" id="file" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary">
+                        <i class="fa fa-upload"></i> Upload
+                    </button>
+                </div>
           </form>
-        </div>
-        <!-- /.box-body -->
   </div>
 @endsection
